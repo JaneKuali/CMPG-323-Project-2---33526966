a085a8bd60973a3f747d1f53875b8a901aa45281
Jane Mpho Kuali <janemphokuali@gmail.com>
2022-09-06 15:26:06 +0200

    adding swagger

--- API_Project_2/Startup.cs
+++ API_Project_2/Startup.cs
@@ -17,6 +17,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication;
 using System.Text;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 
 namespace API_Project_2
 {
@@ -35,7 +37,35 @@ namespace API_Project_2
             services.AddControllers();
 
             // For Entity Framework 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnStr")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer("name=ConnectionStrings:ConnStr"));
+
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Title = "JWTToken_Auth_API",
+                    Version = "v2"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer'[space] and then your token in the text inputbelow.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement { 
+                    {new OpenApiSecurityScheme {
+                        Reference = new OpenApiReference {
+                            Type = ReferenceType.SecurityScheme, Id = "Bearer"
+                        }
+                    },
+                        new string[] {}
+                    }
+                });
+            });
 
             // For Identity 
             services.AddIdentity<ApplicationUser, IdentityRole>()
@@ -85,6 +115,11 @@ namespace API_Project_2
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json",
+            "API_Project_2"));
+
         }
     }
 }
